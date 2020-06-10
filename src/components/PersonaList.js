455501e5fdab4d0fde643e657a9d2c752cc6a70e
Manrique <manri.arf15@gmail.com>
2020-06-09 18:26:08 -0700

    fix

--- src/components/PersonaList.js
+++ src/components/PersonaList.js
@@ -1,52 +1,52 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import PersonaDataService from "../services/PersonaService";
- 
-const PersonaList = () =>{
- const [personas, setPersonas] = useState([]);
- const [currentPersona, setCurrentPersona] = useState(null);
- const [currentIndex, setCurrentIndex] = useState(-1);
- 
- const retrivePersonas = () =>{
- PersonaDataService.getAll()
- .then (response => {
- setPersonas(response.data);
- console.log(response.data);
- })
- .catch( e=>{
- console.log(e);
- })
- };
- 
- const setActivePersona = (persona, index) =>{
- setActivePersona(persona);
- setCurrentIndex(index);
- };
- 
- return (
- <div className="list row">
- <div className="col-md-6">
- <h4>Lista de Personas</h4>
- 
- <ul className="list-group">
- {personas &&
- personas.map((persona, index) => (
- <li
- className={
- "list-group-item " + (index === currentIndex ? "active" : "")
- }
- onClick={() => setActivePersona(persona, index)}
- key={index}
- >
- {persona.nombre}
- </li>
- ))}
- </ul>
- 
- 
- </div>
- </div>
- 
- );
- 
+
+const PersonaList = () => {
+    const [personas, setPersonas] = useState([]);
+    const [currentPersona, setCurrentPersona] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(-1);
+
+    const retrivePersonas = () => {
+        PersonaDataService.getAll()
+            .then(response => {
+                setPersonas(response.data);
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    };
+    useEffect(()=>{retrivePersonas();},[]);
+    const setActivePersona = (persona, index) => {
+        setCurrentPersona(persona);
+        setCurrentIndex(index);
+    };
+
+    return (
+        <div className="list row">
+            <div className="col-md-6">
+                <h4>Lista de Personas</h4>
+
+                <ul className="list-group">
+                    {personas &&
+                        personas.map((persona, index) => (
+                            <li
+                                className={
+                                    "list-group-item " + (index === currentIndex ? "active" : "")
+                                }
+                                onClick={() => setActivePersona(persona, index)}
+                                key={index}
+                            >
+                                {persona.nombre}
+                            </li>
+                        ))}
+                </ul>
+
+
+            </div>
+        </div>
+
+    );
+
 };
 export default PersonaList;
